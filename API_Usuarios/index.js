7ff1b58a6375a40e1ef2e60a44c7cfe2a5cd2625
Diovana Manica <dhiovana_pires@hotmail.com>
2024-09-23 22:07:26 -0300

    API

--- API_Usuarios/index.js
+++ API_Usuarios/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-// baseUrl = http://localhost:8080
+// baseUrl = http://localhost:8080/cadastro
 
 const app = express();
 
@@ -16,17 +16,61 @@ const clientes = [{
 
 
 app.post("/cadastro", (request, response) => {
+    console.log(request.body.id);
 
-    clientes.push(request.body);
+    if(!request.body.id){
+        return response.status(400).send("É obrigatorio enviar o campo id");
+    }else if(!request.body.nome){
+        return response.status(400).send("É obrigatorio enviar o campo nome");
+    }else if(!request.body.email){
+        return response.status(400).send("É obrigatorio enviar o campo email");
+    }else if(!request.body.senha){
+        return response.status(400).send("É obrigatorio enviar o campo senha");
+    }
     
-    response.status(200).send(clientes);
+
+    //for (let i=0; i < clientes.length; i++){
+      //  if (clientes[i].id == id){
+        //    return response.status(404).send("O codigo ${id} ja esta em uso!")
+        //}
+    //}
+
+    //clientes.filter((item)=>{
+      //  if (item.id == id){
+          //  return response.status(404).send(`O codigo ${id} ja esta em uso!`)
+      //  }
+   // });
+   const existe_id = clientes.filter(item => item.id == id);
+   if(existe_id){
+    return response.status(404).send(`O codigo ${id} ja esta em uso!`)
+   }else{
+    clientes.push(request.body);
+   }
+
+
+    
+   return response.status(200).send(clientes);
 
 })
 
 app.get("/consulta", (request, response) => {
-    response.status(200).send(clientes);
+    return response.status(200).send(clientes);
+})
+
+app.get("/cliente/:id([0-9]+)", (request, response) => {
+    const {id} = request.params;
+    const cliente = clientes.filter(item => item.id == id)
+
+    return response.send(cliente);
+
+})
+
+app.delete("/cliente/:id([0-9])", (request, response)=>{
+    const {id} = request.params,
 })
 
 app.listen(8080, () => {
-    console.log("O servidor esta rodando na porta 8080");
-})
+    console.log("O servidor está rodando na porta 8080");
+});
+
+//criar um filtro que retorne o cliente com base no parametro desse codigo
