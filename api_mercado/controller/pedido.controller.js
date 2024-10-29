8f2225afce9ebe43b6288627c45bf11ad29ec924
Diovana Manica <dhiovana_pires@hotmail.com>
2024-10-28 22:12:56 -0300

    Mercado

--- api_mercado/controller/pedido.controller.js
+++ api_mercado/controller/pedido.controller.js
@@ -3,7 +3,7 @@ const conn = require("../mysql-connection");
 //crud
 module.exports = ({
     //cadastro: (req, res)=> {},
-    consultar: async (req, res) => {},
+    consultar: async (req, res) => {
 
         try{
             const data = await conn.raw("SELECT * FROM PRODUTO");
@@ -12,8 +12,8 @@ module.exports = ({
             console.log(erro);
             return res.status(500).send ({ msg: "erro ao consultar os produtos!"});
         }
+    }
 
     //atualizar: (req, res)=>{},
     //delete: (req, res)=>{}
 });
-
