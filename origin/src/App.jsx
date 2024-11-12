50ed00a9d2c163eb6d1bb6c61065c77207e28a16
Diovana Manica <dhiovana_pires@hotmail.com>
2024-11-11 21:51:41 -0300

    Origin

---
+++ origin/src/App.jsx
@@ -0,0 +1,13 @@
+import Teste from "./Teste.jsx"
+
+function App() {
+  
+  return (
+    <>
+   <h1> Ola Mundo!</h1>
+    <Teste/>
+   </>
+  )
+}
+
+export default App
---
+++ origin/src/Teste.jsx
@@ -0,0 +1,8 @@
+import "./Teste.css"
+
+function Teste(){
+    return(
+        <h2>Componete 2</h2>
+    )
+}
+export default Teste
